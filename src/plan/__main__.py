cb57446387439af116d5fa440c0d0fc8ee702cf9
Nick <nikolai.in@tuta.io>
2023-01-16 00:17:22 +0300

    ♻️ Forgot to remove duplicate code

--- src/plan/__main__.py
+++ src/plan/__main__.py
@@ -4,31 +4,6 @@ from shapely import geometry, affinity
 
 from utils.utils import *
 
-#  Подключим константы API Компас
-kompas6_constants = gencache.EnsureModule("{75C9F5D0-B5B8-4526-8681-9903C567D2ED}", 0, 1, 0).constants
-kompas6_constants_3d = gencache.EnsureModule("{2CAF168C-7961-4B90-9DA2-701419BEEFE3}", 0, 1, 0).constants
-
-#  Подключим описание интерфейсов API5
-kompas6_api5_module = gencache.EnsureModule("{0422828C-F174-495E-AC5D-D31014DBBE87}", 0, 1, 0)
-kompas_object = kompas6_api5_module.KompasObject(
-    Dispatch("Kompas.Application.5")._oleobj_.QueryInterface(kompas6_api5_module.KompasObject.CLSID,
-                                                             pythoncom.IID_IDispatch))
-miscHelpers.iKompasObject = kompas_object
-
-#  Подключим описание интерфейсов API7
-kompas_api7_module = gencache.EnsureModule("{69AC2981-37C0-4379-84FD-5DD2F3C0A520}", 0, 1, 0)
-application = kompas_api7_module.IApplication(
-    Dispatch("Kompas.Application.7")._oleobj_.QueryInterface(kompas_api7_module.IApplication.CLSID,
-                                                             pythoncom.IID_IDispatch))
-miscHelpers.iApplication = application
-
-documents = application.Documents
-
-#  Получим активный документ
-kompas_document = application.ActiveDocument
-kompas_document_2d = kompas_api7_module.IKompasDocument2D(kompas_document)
-iDocument2D = kompas_object.ActiveDocument2D()
-
 ONE_TO_SCALE = 2000
 
 
